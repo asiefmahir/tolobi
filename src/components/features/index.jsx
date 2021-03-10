d70477c4815e901be3570fb6923a31d5af784253
mahbubsk <skmahbubkr787@gmail.com>
2021-03-10 19:55:47 +0600

    featured item css done

--- src/components/features/index.jsx
+++ src/components/features/index.jsx
@@ -17,13 +17,15 @@ const H3 = styled.h3`
 `;
 const CardHolder = styled.div`
 	display: flex;
+	align-items:center;
+	justify-content:space-around;
 `;
 const Features = ({ features }) => {
 	return (
 		<Container>
 			<H6>Real Estate Agents</H6>
 			<H3>Who Use Tolobi</H3>
-			<CardHolder>
+			<CardHolder className="container">
 				{features.map((feature) => (
 					<ItemCard card={feature} />
 				))}
