42e7406bb184b4cb7aa88ef7fc2a1c23dc1dce9d
mahbubsk <skmahbubkr787@gmail.com>
2021-03-11 14:52:49 +0600

    competitor section done without border-bottom

--- src/sections/competitor-table/Row.jsx
+++ src/sections/competitor-table/Row.jsx
@@ -1,22 +1,23 @@
 const Row = ({ rowData }) => {
 	const { title, competitor, tolobi } = rowData;
 	return (
-		<tr>
-			<td>{title}</td>
+		<tr className="tr">
+			<td className="table-row-item">{title}</td>
 
 			{typeof competitor !== "object" ? (
-				<td>{competitor}</td>
+				
+				<td className="table-row-item"><h2>{competitor}</h2></td>
 			) : (
-				<td>
-					<p>{competitor?.percentage}</p>
+				<td className="table-row-item">
+					<h2>{competitor?.percentage}</h2>
 					<p>{competitor?.condition}</p>
 				</td>
 			)}
 			{typeof tolobi !== "object" ? (
-				<td>{competitor}</td>
+				<td className="table-row-item"><h2>{competitor}</h2></td>
 			) : (
-				<td>
-					<p>{tolobi?.percentage}</p>
+				<td className="table-row-item">
+					<h2>{tolobi?.percentage}</h2>
 					<p>{tolobi?.condition}</p>
 				</td>
 			)}
