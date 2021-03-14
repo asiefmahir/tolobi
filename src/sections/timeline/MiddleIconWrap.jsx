2b764abb171ef9718574827542ca735b86710d83
mahbubsk <skmahbubkr787@gmail.com>
2021-03-14 16:18:22 +0600

    responsiveness done for timeline

--- src/sections/timeline/MiddleIconWrap.jsx
+++ src/sections/timeline/MiddleIconWrap.jsx
@@ -2,8 +2,23 @@ import styled from 'styled-components';
  
 
 const MiddleIconWrap = styled.div`
-    margin-top: -4px;
-    margin-left: 25%;
+    &{
+        margin-top: -4px;
+        margin-left: 27%;
+    }
+
+
+    @media (max-width: 1024px) {
+        & {
+            margin-left: 27%;
+        }
+    }
+
+    @media (max-width: 768px) {
+        & {
+            margin-left: 20.5%;
+        }
+    }
 `
 
 export default MiddleIconWrap;
