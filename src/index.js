853e15d6b6afe6920f620b1642073e337ccb0bfd
mahbubsk <skmahbubkr787@gmail.com>
2021-03-14 17:12:21 +0600

    timeline curve section width updated at 100%

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import './responsive.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
